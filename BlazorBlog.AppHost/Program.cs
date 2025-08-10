f0fd171b799bb316a0e5727b5cacaca547993c65
Hein Taljaard <hein.taljaard@gmail.com>
2025-08-10 18:26:51 +1000

    initial commit

---
+++ BlazorBlog.AppHost/Program.cs
@@ -0,0 +1,24 @@
+using Projects;
+
+var builder = DistributedApplication.CreateBuilder(args);
+
+
+var postgres = builder.AddPostgres("postgres")
+                      .WithContainerName("blogdb_postgres")
+                      .WithLifetime(ContainerLifetime.Persistent);
+
+
+var dbName = "blogdb";
+var creationScript = $$"""
+    -- Create the database
+    CREATE DATABASE {{dbName}};
+
+    """;
+
+var blogDb = postgres.AddDatabase(dbName)
+                     .WithCreationScript(creationScript);
+
+var webUi = builder.AddProject<Projects.BlazorBlog_UI>("BlazorUI")
+                   .WithReference(blogDb);
+
+builder.Build().Run();
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# BlazorBlog
-Blog site using Bazor and Aspire. 
+# Introdocution
+This project is an exersice in using .NET Aspire, Blazor and Azure AI Services to build a blog site. 
+
+As I use MUI (MudBlazor) in most of my projects, I wanted to use tailwind css as I have never used it before.
+
+# Technologies
+- SQL Server + EF Core 
+- .NET 9
+- Blazore SSR
+- Tailwind CSS
